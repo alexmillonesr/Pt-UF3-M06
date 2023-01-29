82db24aa85c32f76d5bff13f3a09283e9cc4d331
Alex M.R <alexmillonesr@gmail.com>
2023-01-29 02:00:27 +0100

    routes created

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EventsComponent } from './components/events/events.component';
 
-const routes: Routes = [];
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+
+const routes: Routes = [
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'register',
+    component: RegisterComponent
+  },
+  {
+    path: 'events',
+    component: EventsComponent
+  },
+  {
+    path: '**',
+    redirectTo: 'login'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
